018a035d80512c222c15454305537fc2f20bced5
Gabriel Horner <gabriel.horner@gmail.com>
2011-10-03 00:21:09 -0400

    tweaks

--- lib/keys/runner.rb
+++ lib/keys/runner.rb
@@ -2,17 +2,18 @@ class Keys::Runner < Thor
   method_options :plugins_dir => 'plugins', %w{--field -f} => 'key'
   desc 'list [QUERY]', 'List keys'
   def list(query=nil)
-    results = query.nil? ? keys : begin
-      keys.select {|e| e[options[:field].to_sym] =~ /#{query}/ }
-    end
+    file = generate_map_file
+    keys = parse_map_file(file)
+    keys.select! {|e| e[options[:field].to_sym] =~ /#{query}/ } if query
+
     require 'hirb'
-    puts Hirb::Helpers::Table.render(results,
-     :fields => [:key, :mode, :plugin, :file], :sort => options[:field].to_sym)
+    puts Hirb::Helpers::Table.render(keys,
+     :fields => [:key, :mode, :plugin, :action], :sort => options[:field])
   end
 
   private
 
-  def generate_keymap_file
+  def generate_map_file
     require 'tempfile'
     file = Tempfile.new('key').path
     system %[ vim -c 'colorscheme default | redir! > #{file} | ] +
@@ -20,8 +21,7 @@ class Keys::Runner < Thor
     file
   end
 
-  def keys
-    file = generate_keymap_file
+  def parse_map_file(file)
     lines = File.read(file).strip.split("\n")
     lines.slice_before {|e| e !~ /Last set/ }.map do |arr|
       key = {}
@@ -33,6 +33,7 @@ class Keys::Runner < Thor
       key[:file]   = file if file
 
       key[:key]  = arr[0][/^\S*\s+(\S+)/, 1]
+      key[:action] = arr[0][/^\S*\s+\S+\s+(.*)$/, 1]
       key[:mode] = arr[0][/^[nvosx]+/] || 'nvosx'
       key
     end.compact
