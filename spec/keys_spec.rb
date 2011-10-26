eb76c277087e218e5470720cedf6b7772e40b4ae
Gabriel Horner <gabriel.horner@gmail.com>
2011-10-26 05:25:49 -0400

    move common search options to its own spec

--- spec/keys_spec.rb
+++ spec/keys_spec.rb
@@ -11,32 +11,6 @@ describe "vimdb keys" do
     stdout.must_match /1 row/
   end
 
-  it "with --field option searches another field" do
-    vimdb 'keys', 'L-z' ,'-f=desc'
-    stdout.must_match /0 rows/
-  end
-
-  it "with --reverse_sort option reverse sorts field" do
-    vimdb 'keys', '-R'
-    stdout.must_match /gq.*'\]/m
-  end
-
-  it "with --regexp option converts search to regexp" do
-    vimdb 'keys', '^C-', '-r'
-    stdout.wont_match /0 rows/
-    stdout.must_match /C-a.*C-z/m
-  end
-
-  it "with --ignore-case option ignores case" do
-    vimdb 'keys', 'Q:', '-i'
-    stdout.must_match /q:.*1 row/m
-  end
-
-  it 'with --mode option ANDs mode to search' do
-    vimdb 'keys', 'gm', '-m=n'
-    stdout.must_match /1 row/
-  end
-
   describe "edge cases" do
     it "converts control key to C-" do
       vimdb 'keys', 'C-z'
